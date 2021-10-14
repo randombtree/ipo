a9c0e36f7727df1dc851a01ef0a0c1a9f7e6e0e6
Roger Blomgren <roger.blomgren@iki.fi>
2021-10-14 11:01:27 +0300

    Dynamically load cmd_* modules and add commands.

--- ipo/command.py
+++ ipo/command.py
@@ -3,15 +3,10 @@ Parse 'pio' command line.
 
 Acts as an entry-point for all subcommands.
 """
-
+import sys
 import argparse
-
-# TODO: Dynamically probe for cmd_*?
-from . import cmd_daemon
-from . import cmd_misc
-from . import cmd_container
-
-SUBCOMMANDS = [cmd_daemon, cmd_misc, cmd_container]
+import pkgutil
+import importlib
 
 
 def main():
@@ -25,8 +20,18 @@ def main():
         description = 'Python ICON orchestrator command line interface'
     )
     subparser = parser.add_subparsers(dest = 'command', help = 'sub-command help')
-    for m in SUBCOMMANDS:
-        m.add_subcommand(subparser)
+
+    # Dynamically load cmd_* modules
+    package = sys.modules[__name__].__package__
+    pmod = sys.modules[package]
+    for m in pkgutil.iter_modules(path = pmod.__path__):
+        if m.ispkg is False and m.name.startswith('cmd_'):
+            module = importlib.import_module(f'.{m.name}', 'ipo')
+            if not hasattr(module, 'add_subcommand'):
+                print(f'WARNING: {m.name} missing add_subcommand and will not work!')
+            else:
+                module.add_subcommand(subparser)
+
     args = parser.parse_args()
     # The subparsers add entry points to the namespace in the form of a callable function
     if 'func' in args:
