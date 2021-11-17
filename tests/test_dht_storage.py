442b3ec2557f6275ead18e6af654e5717c0b9892
Roger Blomgren <roger.blomgren@iki.fi>
2021-11-17 09:06:20 +0200

    Improve dht storage testing. - Don't add values in sorted order, to test the insertion logic. - Test appending value first and last. - Also test use (test) get().

--- tests/test_dht_storage.py
+++ tests/test_dht_storage.py
@@ -14,30 +14,45 @@ REFTIME = time.time_ns()
 HOUR = 60 * 60 * 10 ** 9
 WEEK = 7 * 24 * HOUR
 
-def make_ip(a = 10, b = 1, c = 2, d = 1):
+
+def make_ip(a = 10, b = 1, c = 2, d = 1) -> bytes:  # pylint: disable: invalid-name
+    """ Create a packed IP address """
     return bytes([a, b, c, d])
+
+
+def sort_metrics(metrics: dict[bytes, list[DistanceMetric]]) -> dict[bytes, list[DistanceMetric]]:
+    """ Sort the metrics list """
+    new_dict: dict[bytes, list[DistanceMetric]] = {}
+    for k, v in metrics.items():
+        new_dict[k] = list(sorted(v, key = lambda metric: metric.rtt))
+    return new_dict
+
 
 class TestStorage(IsolatedAsyncioTestCase):
     """ Test the custom kademlia storage """
+    # Test metrics in unsorted order to simulate real inserts
     TEST_METRICS = {
-        1: [
+        make_ip(b = 2): [
+            DistanceMetric(20, 3, make_ip(c = 3), 1337, REFTIME - 2 * HOUR),
             DistanceMetric(6,  2, make_ip(c = 1), 1337, REFTIME - HOUR),     # noqa: E241
             DistanceMetric(10, 3, make_ip(c = 2), 1337, REFTIME - HOUR),
-            DistanceMetric(20, 3, make_ip(c = 3), 1337, REFTIME - 2 * HOUR),
         ],
-        2 : [
-            DistanceMetric(5,  2, make_ip(c = 4), 1337, REFTIME - HOUR),     # noqa: E241
+        make_ip(b = 4) : [
             DistanceMetric(15, 3, make_ip(c = 5), 1337, REFTIME - HOUR),
             DistanceMetric(25, 3, make_ip(c = 6), 1337, REFTIME - 2 * HOUR),
+            DistanceMetric(5,  2, make_ip(c = 4), 1337, REFTIME - HOUR),     # noqa: E241
         ]
     }
+
+    SORTED_METRICS = sort_metrics(TEST_METRICS)
 
     async def asyncSetUp(self):
         """ storage need running loop """
         self.storage = RouteStorage()  # pylint: disable=attribute-defined-outside-init
         # Set some default values
-        for k, v in self.TEST_METRICS.items():
-            self.storage[k] = pack_metrics(v)
+        for k, metrics in self.TEST_METRICS.items():
+            for metric in metrics:
+                self.storage[k] = pack_metrics([metric])
 
     def validate_metrics(self, key, comparison: list[DistanceMetric]):
         """ compare storage metrics to supplied comparison """
@@ -49,32 +64,53 @@ class TestStorage(IsolatedAsyncioTestCase):
     def test_contents(self):
         """ Test that the set-up contents are stored correctly """
         self.storage.cull()  # Might as well test it here
-        for k, v in self.TEST_METRICS.items():
+        for k, v in self.SORTED_METRICS.items():
             self.validate_metrics(k, v)
+            self.validate_metrics(k, unpack_metrics(self.storage.get(k)))
+
+    def test_append_first(self):
+        """ Test appending first in list """
+        KEY = next(self.SORTED_METRICS.keys().__iter__())
+        metrics = self.SORTED_METRICS[KEY].copy()
+        new = DistanceMetric(5, 1, make_ip(d = 10), 8888, REFTIME - HOUR)
+        metrics.insert(0, new)
+        self.storage[KEY] = pack_metrics([new])
+        self.validate_metrics(KEY, metrics)
+
+    def test_append_last(self):
+        """ Test append last in list """
+        KEY = next(self.SORTED_METRICS.keys().__iter__())
+        metrics = self.SORTED_METRICS[KEY].copy()
+        new = DistanceMetric(40, 1, make_ip(d = 10), 8888, REFTIME - HOUR)
+        metrics.append(new)
+        self.storage[KEY] = pack_metrics([new])
+        self.validate_metrics(KEY, metrics)
 
     def test_append(self):
         """ Test appending a metric to an existing collection """
-        KEY = 2
-        metrics = list(self.TEST_METRICS[KEY])
+        it = self.SORTED_METRICS.keys().__iter__()
+        _KEY1 = next(it)
+        KEY2 = next(it)
+        metrics = list(self.SORTED_METRICS[KEY2])
         new_metric = DistanceMetric(10, 3, make_ip(d = 10), 1337, REFTIME)
         metrics.insert(1, new_metric)  # As second item
         # And append to store
-        self.storage[KEY] = pack_metrics([new_metric])
+        self.storage[KEY2] = pack_metrics([new_metric])
         # And validate
-        self.validate_metrics(KEY, metrics)
+        self.validate_metrics(KEY2, metrics)
 
     def test_cull(self):
         """ Test if cull purges the old entries as it should """
         with time_patch as tp:
             # Let one week pass
             tp.return_value = REFTIME + WEEK + HOUR
-            new_metrics = pack_metrics([DistanceMetric(7, 3, make_ip(d = 10), 1337, REFTIME + WEEK)])
-            KEY = 5
+            new_metrics = pack_metrics([DistanceMetric(40, 3, make_ip(d = 10), 1337, REFTIME + WEEK)])
+            KEY = make_ip(c = 100)
             self.storage[KEY] = new_metrics
             self.storage.cull()
             tp.assert_called()
             # The "old" values should be deprecated
-            for k in self.TEST_METRICS:
+            for k in self.SORTED_METRICS:
                 self.assertIsNone(self.storage.get(k), f'Key {k} is still in store??')
 
             metrics = self.storage[KEY]
