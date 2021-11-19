e0d49ebc11a7aadbf07e6d433000032d3997525b
Roger Blomgren <roger.blomgren@iki.fi>
2021-11-19 11:43:27 +0200

    Convert client to use dispatcher.

--- ipo/client/iconclient.py
+++ ipo/client/iconclient.py
@@ -2,22 +2,15 @@
 from enum import Enum, auto
 import asyncio
 from asyncio import Task, Queue
-import json
 import logging
 
-from typing import Union
+from typing import Optional
 
-from ..util.asynctask import AsyncTaskRunner
 from ..util.signal import Signal, Emitter
 from ..api import message
-from ..api.message import MessageReader, JSONWriter
-
+from ..daemon.messagetask import MessageTaskDispatcher
 
 log = logging.getLogger(__name__)
-
-
-class ShutdownEvent:
-    ...
 
 
 class ClientState(Enum):
@@ -35,9 +28,8 @@ class IconClient(Emitter):
 
     sockname: str
     inqueue: Queue
-    _task: Union[Task, None]
+    _task: Optional[Task]
     state: ClientState
-    task_runner: AsyncTaskRunner
 
     def __init__(self, sockname = '/run/icond/icon.sock'):
         """
@@ -47,7 +39,6 @@ class IconClient(Emitter):
         self.sockname = sockname
         self._task = None
         self.state = ClientState.DISCONNECTED
-        self.task_runner = AsyncTaskRunner()
         self.inqueue = Queue()
 
     async def connect(self):
@@ -69,82 +60,39 @@ class IconClient(Emitter):
         """ Disconnect from orchestrator """
         if self.state in (ClientState.CONNECTING, ClientState.CONNECTED):
             self.state = ClientState.SHUTDOWN
-            # _nowait to guarantee no shutdown race
-            self.inqueue.put_nowait(ShutdownEvent())
-            await asyncio.wait((self._task,))
+            await self._task.cancel()
+
+    async def _communicate(self, reader, writer):
+        # We abuse the MessageTaskDispatcher because it takes care of most of
+        # the communication woes
+        async with MessageTaskDispatcher(reader, writer, {}, None) as dispatcher:
+            await dispatcher.write(message.ClientHello(version = '0.0.1'))
+            async for unhandled, _out in dispatcher:
+                if not self.state == ClientState.CONNECTED:
+                    if isinstance(unhandled, message.HelloReply):
+                        log.debug('Handshake with orchestrator completed version=%s',
+                                  unhandled.version)
+                        # TODO: Version etc.
+                        self.state = ClientState.CONNECTED
+                        await self.Connected()
+                        continue
+                log.error('Unhandled %s', unhandled)
+
+        if self.state == ClientState.CONNECTED:
+            log.debug('Disconnected..')
+            self.state = ClientState.DISCONNECTED
+            await self.Disconnected()
 
     async def _run(self):
-        inqueue_task = self.task_runner.run(self.inqueue.get)
-        connect_task = self.task_runner.run(asyncio.sleep(0))
-        timeout_task = None
-        read_task    = None
-        writer       = None
-        async for task in self.task_runner.wait_next():
-            if task == connect_task:
-                # Connection task is either a timeout return or a result from the connection attempt
-                e = task.exception()
-                if e:
-                    # Connection failed
-                    # Try again in 5 secs..
-                    log.warning('Connection to server failed')
-                    connect_task = self.task_runner.run(asyncio.sleep(5))
-                    continue
-                r = task.result()
-                if r is None:
-                    log.warning('Timeout; Try to connect to server')
-                    # This is after a delay.. try to connect
-                    connect_task = self.task_runner.run(
-                        asyncio.open_unix_connection(path = self.sockname),
-                    )
-                else:
-                    (reader, writer) = r
-                    reader = MessageReader(reader)
-                    writer = JSONWriter(writer)
-                    log.debug('Sending handshake')
-                    await writer.write(message.ClientHello(version = '0.0.1'))
-                    try:
-                        # Wait for the appropriate reply
-                        # TODO: Move to appropriate place as this by-passes the task runner
-                        reply = await reader.read()
-                        if isinstance(reply, message.HelloReply):
-                            # TODO: Validation..
-                            read_task = self.task_runner.run(
-                                reader.read
-                            )
-                            log.debug('Connected')
-                            self.state = ClientState.CONNECTED
-                            await self.Connected()
-                            continue
-                    except (json.JSONDecodeError, message.InvalidMessage) as e:
-                        log.error('Error decoding message %s: %s', e.__class__.__name__, e)
-                        ...
-                    reader.close()
-                    writer.close()
-                    log.error('Connection handshake failed')
-                    connect_task = self.task_runner.run(asyncio.sleep(10))
-            elif task == read_task:
-                # Is there something to read
-                exception = task.exception()
-                if exception:
-                    # Read failed; just throw away this connection and try again
-                    self.state = ClientState.CONNECTING
-                    await self.Disconnected()
-                    self.task_runner.remove_task(read_task)
-                    # Start reconnecting ASAP
-                    connect_task = self.task_runner.run(asyncio.sleep(0))
-                    continue
-                msg = task.result()
-                log.debug('ICON server sent us %s', msg)
-                # TODO..
-            elif task == inqueue_task:
-                r = task.result()
-                self.inqueue.task_done()
-                if isinstance(r, ShutdownEvent):
-                    log.debug('Client will disconnect')
-                    break
-            else:
-                log.error('BUGBUG! Unhandled task {task}')
-        self.state = ClientState.DISCONNECTED
-        await self.Disconnected()
-        self.task_runner.clear()
-
+        while not self.state == ClientState.SHUTDOWN:
+            try:
+                log.debug('Connecting to orchestrator..')
+                self.state = ClientState.CONNECTING
+                (reader, writer) = await asyncio.open_unix_connection(path = self.sockname)
+                log.debug('Connection established to orchestrator')
+            except OSError:
+                log.warning('Connection to orchestrator failed')
+                await asyncio.sleep(5)
+                continue
+            await self._communicate(reader, writer)
+            await asyncio.sleep(5)
